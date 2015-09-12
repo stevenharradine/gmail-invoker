ad6e8eceef04ed3673c4d87e2fc46a5fb13a010b
steven harradine <steven.harradine@telus.com>
2015-09-12 16:20:07 -0400

    added scripts folder and hello world example

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports.CODEWORD       = "codeword"
-module.exports.SCRIPT         = "bash ./script.sh"
+module.exports.SCRIPT         = "bash ./scripts/hello.sh"
 module.exports.EMAIL_PROVIDER = 'Gmail'
 module.exports.EMAIL_USER     = 'youremail'
 module.exports.EMAIL_PASSWORD = 'yourpassword'
