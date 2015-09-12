1aa9215728a4d9f038258b3d2dedc23c665d29fc
steven harradine <steven.harradine@telus.com>
2015-09-12 17:17:07 -0400

    read codeword, script and email list from args

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-module.exports.CODEWORD       = "codeword"
-module.exports.SCRIPT         = "bash ./scripts/hello.sh"
+module.exports.CODEWORD       = process.argv[2]
+module.exports.SCRIPT         = "bash " + process.argv[3]
 module.exports.EMAIL_PROVIDER = 'Gmail'
 module.exports.EMAIL_USER     = 'youremail'
 module.exports.EMAIL_PASSWORD = 'yourpassword'
-module.exports.EMAIL_LIST     = ['your@email.com']
+module.exports.EMAIL_LIST     = process.argv.splice(4)
